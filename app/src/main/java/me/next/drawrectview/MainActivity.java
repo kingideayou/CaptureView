4d86d4104cbb1d1de19096e97b8a7a8725d5f571
NeXT___ <kingideayou@foxmail.com>
2017-01-14 12:00:21 +0800

    feature: 裁图页面调整为全屏模式

--- app/src/main/java/me/next/drawrectview/MainActivity.java
+++ app/src/main/java/me/next/drawrectview/MainActivity.java
@@ -1,8 +1,10 @@
 package me.next.drawrectview;
 
 import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
 
         PaletteView paletteView = (PaletteView) findViewById(R.id.pv_board);
         paletteView.setOnButtonClickListener(new OnButtonClickListener() {
